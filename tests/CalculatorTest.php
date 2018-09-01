9a10a504c938f2f9505294901ef6eb6318951040
Daniel Smith <hello@dansmithwebdev.com>
2018-09-01 18:26:42 +0100

    Tidy up tests

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
+    public function setUp()
+    {
+        $this->calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
+    }
+    
     public function testInitialisation()
     {
         $calculator = new \Widgets\Calculator([250, 1000, 500, 2000, 5000, 5000, 1000, 250, 1000, 'Foo', 'Bar']);
@@ -14,77 +19,62 @@ class CalculatorTest extends TestCase
 
     public function testCalculation1()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
 
         $this->assertEquals([
             250  => 1,
-        ], $calculator->calculateRequirements(1));
+        ], $this->calculator->calculateRequirements(1));
     }
 
     public function testCalculation250()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             250  => 1,
-        ], $calculator->calculateRequirements(250));
+        ], $this->calculator->calculateRequirements(250));
     }
 
     public function testCalculation251()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             500  => 1,
-        ], $calculator->calculateRequirements(251));
+        ], $this->calculator->calculateRequirements(251));
     }
 
     public function testCalculation499()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             500  => 1,
-        ], $calculator->calculateRequirements(499));
+        ], $this->calculator->calculateRequirements(499));
     }
 
     public function testCalculation501()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             500  => 1,
             250  => 1,
-        ], $calculator->calculateRequirements(501));
+        ], $this->calculator->calculateRequirements(501));
     }
 
     public function testCalculation751()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             500  => 2,
-        ], $calculator->calculateRequirements(751));
+        ], $this->calculator->calculateRequirements(751));
     }
 
     public function testCalculation999()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             500  => 2,
-        ], $calculator->calculateRequirements(999));
+        ], $this->calculator->calculateRequirements(999));
     }
 
 
     public function testCalculation12001()
     {
-        $calculator = new \Widgets\Calculator([250, 500, 1000, 2000, 5000]);
-
         $this->assertEquals([
             5000 => 2,
             2000 => 1,
             250  => 1,
-        ], $calculator->calculateRequirements(12001));
+        ], $this->calculator->calculateRequirements(12001));
     }
 }
